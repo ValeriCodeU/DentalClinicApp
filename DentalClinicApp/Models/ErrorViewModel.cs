24162805b0504a8587670c4962625fe5f9b504da
ValeriCodeU <valeri.yanev@icloud.com>
2022-11-05 23:51:31 +0200

    Add project files.

---
+++ DentalClinicApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DentalClinicApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
