b0ce24fae52ed0e4e74ec82f9f6b477d2c9a6cb5
ValeriCodeU <valeri.yanev@icloud.com>
2022-12-16 20:42:33 +0200

    Updated admin layout

---
+++ DentalClinicApp.Core/Models/Patients/PatientCardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalClinicApp.Core.Models.Patients
+{
+    internal class PatientCardViewModel
+    {
+    }
+}
---
+++ DentalClinicApp.Test/PatientServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalClinicApp.Test
+{
+	public class PatientServiceTest
+	{
+        [SetUp]
+
+        public void Setup()
+        {
+        }
+
+        [TearDown]
+
+        public void TearDown()
+        {
+
+        }
+    }
+}
