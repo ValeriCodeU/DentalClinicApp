026b56c2a4be181b7aaa256629f1cae3bdc3f40a
ValeriCodeU <valeri.yanev@icloud.com>
2023-04-05 23:57:57 +0300

    Updated interface for dentist data

--- DentalClinicApp.Core/Contracts/IDentistService.cs
+++ DentalClinicApp.Core/Contracts/IDentistService.cs
@@ -7,18 +7,52 @@ using System.Threading.Tasks;
 
 namespace DentalClinicApp.Core.Contracts
 {
+    /// <summary>
+    ///  Manipulates dentist data
+    /// </summary>
     public interface IDentistService
     {
+        /// <summary>
+        /// Get all managed dentists
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>List of dentists</returns>
         Task<DentistDetailsViewModel> GetAllManagedDentistsAsync(Guid userId);
 
+        /// <summary>
+        /// Check if the dentist exists
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>Boolean data type if dentist exists</returns>
         Task<bool> IsExistsByIdAsync(Guid userId);
 
+        /// <summary>
+        /// Get dentist Id
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>Integer Id for dentist</returns>
         Task<int> GetDentistIdAsync(Guid userId);
 
+        /// <summary>
+        /// Get statistic for dentist
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>View model for dentist statistic</returns>
         Task<DentistStatisticsViewModel> GetStatisticsAsync(int id);
 
+        /// <summary>
+        /// Create a user as a dentist        
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="managerId"></param>
+        /// <returns>Boolean data type to check for a successful operation</returns>
         Task<bool> AddUserAsDentistAsync(Guid userId, int managerId);
 
+        /// <summary>
+        /// Get manager Id
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>Integer Id for manager</returns>
         Task<int> GetManagerOfDentistAsync(Guid userId);
     }
 }
